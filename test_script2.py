0251b430a2e81bf076dc4cc4349d67b1db018781
abrilrosas926 <abrilrosas.rosario@gmail.com>
2025-02-03 08:26:12 +0000

    Update test_script2.py

--- test_script2.py
+++ test_script2.py
@@ -4,4 +4,4 @@ from script2 import add_numbers
 def test():
     assert add_numbers(1, 2) == 3
     assert add_numbers(10, 20) == 30
-    assert add_numbers(11, -2) == 8
+    assert add_numbers(10, -2) == 8
